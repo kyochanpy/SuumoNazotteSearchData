ce2f1494c9f660ac62e6d4029679d9a155760d40
kyochanpy <nasu.9618.0527@gmail.com>
2024-07-07 15:44:39 +0900

    fix

--- map_ui_data/medical_institution/__main__.py
+++ map_ui_data/medical_institution/__main__.py
@@ -6,15 +6,15 @@ from .main import MedicalInstitution
 
 def main():
     config = load_config()
-    government_offices = MedicalInstitution(
+    medical_institution = MedicalInstitution(
         config,
         DB(config),
         S3(config),
         MedicalInstitutionXmlParser(),
-        "government_offices",
+        "medical_institution",
     )
     for i in range(47):
-        government_offices.run(f"{i+1:0>2}")
+        medical_institution.run(f"{i+1:0>2}")
 
 
 if __name__ == "__main__":
