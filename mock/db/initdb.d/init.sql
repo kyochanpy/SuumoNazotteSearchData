0e93f11e223c28a867216038732cfc3595a0f459
kyochanpy <nasu.9618.0527@gmail.com>
2024-07-15 17:36:08 +0900

    fix: geometry型ってやつにする必要があるらしい

--- mock/db/initdb.d/init.sql
+++ mock/db/initdb.d/init.sql
@@ -10,6 +10,5 @@ CREATE TABLE master (
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    latitude FLOAT NOT NULL,
-    longitude FLOAT NOT NULL
+    location GEOMETRY NOT NULL
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
